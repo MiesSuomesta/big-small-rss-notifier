3cae79193c812c63af0397b0da325925a1736278
Lauri Jakku <lja@iki.fi>
2020-06-20 10:26:12 +0300

    Release 0.2.0

--- setup-test.py
+++ setup-test.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="big-small-rss-notifier-MiesSuomesta", # Replace with your own username
-    version="0.2.0",
+    version="0.2.1",
     author="Lauri Jakku",
     author_email="lja@iki.fi",
     description="RSS Notifier",
